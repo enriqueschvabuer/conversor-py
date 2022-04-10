7206c0bf0c1007bce02f8c3d39bc3dcb2e39d9e6
enriqueschvabuer <enriqueschvabuer@gmail.com>
2022-04-10 18:21:15 -0300

    primer commit conversor de moneda

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input("ingrese cantidad de pesos: ")
+pesos = float(pesos)
+valor_dolar = 196.5
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print ("tienes $" + dolares + " dolares")
